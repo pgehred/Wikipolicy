d70e35a57d3a7af0e5361dd5ce7cb467918c0e82
Heather Akers-Healy <heather@abbynormative.org>
2016-06-30 14:18:27 -0700

    Update replace.js
    new version with expanded names list, link to petition in replacement text

--- Wikipolicy-beta/replace.js
+++ Wikipolicy-beta/replace.js
@@ -1,52 +1,10 @@
-walk(document.body);
+var names=['John Russell "Rusty" Houser',"John Russell 'Rusty' Houser","Rusty Houser","John Houser","Omar Mateen","Noah Harpham","Jason Dalton ","Jiverly Wong","James Holmes","Michael McLendon","Seung-Hui Cho","Adam Lanza","Charles Whitman","Eric Harris","Dylan Klebold","Syed Farook","Syed Rizwan Farook","Tashfeen Malik","Dylann Roof","Wade Page","Nidal Hasan","Aaron Alexis","Chris Harper-Mercer","Robert Dear","Robert Lewis Dear"];
 
-function walk(node)  
-{
-	
-	var child, next;
-
-	switch ( node.nodeType )  
-	{
-		case 1: 
-		case 9:  
-		case 11: 
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling; 
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: 
-			handleText(node);
-			break;
-	}
-}
-
-function handleText(textNode) 
-{
-
-	var v = textNode.nodeValue;
-   
-	v = v.replace(/\bJiverly Wong\b/g, "the Binghamton loser");
-	v = v.replace(/\bJames Holmes\b/g, "the pathetic person of Aurora whose only redeeming quality was inspiring the heroes of #NoNotoriety");
-	v = v.replace(/\bMichael McLendon\b/g, "some pathetic guy we forgot even though he proved a total douchebag only 6 years ago");
-	v = v.replace(/\bSeung-Hui Cho\b/g, "the alienated loser at VaTech (Go Hokies! Screw that idiot!)");
-	v = v.replace(/\bAdam Lanza\b/g, "the mama's boy POS who—other than a rocking good middle name—was a totally worthless loser from the lovely and bucolic Newtown, Connecticut");
-	v = v.replace(/\bCharles Whitman\b/g, "some human failure who was so long ago that he's mostly but not entirely forgotten in cool music scene of Austin");
-	v = v.replace(/\bEric Harris\b/g, "complete and total failure as a person despite attending the fine high school of Columbine, CO (bet he never climbed a 14er)");
-	v = v.replace(/\bDylan Klebold\b/g, "that other complete and total failure as a person who attended Columbine (bus alse never climbed a 14er... loser)");
-	v = v.replace(/\bSyed Farook\b/g, "a browbeaten guy whose own family hated him, and now hate him even more due to his cowardly actions in San Bernardino");
-	v = v.replace(/\bTashfeen Malik\b/g, "some woman, almost certainly annoying, and definitely a worthless human being #byefelicia (Oh: and America, learn to spell: #SanBernardino has an R)");
-	v = v.replace(/\bDylann Roof\b/g, "noted human troll and neo-fascist POS who successfully shamed the lovely Palmetto state to take some good actions that he would have hated (Hey Mississippi, get on board! This is 2015, not 1865) Also... GO CLEMSON TIGERS!)");
-	v = v.replace(/\bWade Page\b/g, "racist so stupid he literally didn't know the difference between Muslims and Sikhs (smooth move, ex-lax)");
-	v = v.replace(/\bNidal Hasan\b/g, "a coward who couldn't handle doing his duty");
-	v = v.replace(/\bAaron Alexis\b/g, "loser who should have just seen a shrink");
-	v = v.replace(/\bChris Harper-Mercer\b/g, "another loser mamma's boy who most notably reminded people that RFK have visited there just 10 days before he was killed a different loser almost fifty years earlier");
-	v = v.replace(/\bRobert Dear\b/g, "ridiculous moron who seems to have an utterly bizarre idea of what the words 'pro' and 'life' mean when put next to each other");
-	v = v.replace(/\bRobert Lewis Dear\b/g, "ridiculous moron who seems to have an utterly bizarre idea of what the words 'pro' and 'life' mean when put next to each other");
-	
-	textNode.nodeValue = v;
+for(var i=0;i<names.length;i++){
+  var regex = new RegExp(names[i],'g');
+  console.log(regex);
+  if(!window.location.href.includes("www.google.com")){
+document.body.innerHTML = document.body.innerHTML.replace(regex, 
+     "<a href='https://www.change.org/p/cnn-don-t-name-the-shooter-save-lives'>[anonymous loser's name replaced to respect victims and prevent future shootings #NoNotoriety]</a>");
+     }
 }

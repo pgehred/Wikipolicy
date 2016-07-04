28014cef0cf7249d8d6b9decf19183316fba5611
Heather Akers-Healy <heather@abbynormative.org>
2016-07-04 10:43:59 -0700

    Update replace.js
    reverted to old method of walking DOM instead of using innerHTML as blunt instrument

--- Wikipolicy-beta/replace.js
+++ Wikipolicy-beta/replace.js
@@ -1,10 +1,41 @@
-var names=['John Russell "Rusty" Houser',"John Russell 'Rusty' Houser","Rusty Houser","John Houser","Omar Mateen","Noah Harpham","Jason Dalton ","Jiverly Wong","James Holmes","Michael McLendon","Seung-Hui Cho","Adam Lanza","Charles Whitman","Eric Harris","Dylan Klebold","Syed Farook","Syed Rizwan Farook","Tashfeen Malik","Dylann Roof","Wade Page","Nidal Hasan","Aaron Alexis","Chris Harper-Mercer","Robert Dear","Robert Lewis Dear"];
+walk(document.body);
 
-for(var i=0;i<names.length;i++){
-  var regex = new RegExp(names[i],'g');
-  console.log(regex);
-  if(!window.location.href.includes("www.google.com")){
-document.body.innerHTML = document.body.innerHTML.replace(regex, 
-     "<a href='https://www.change.org/p/cnn-don-t-name-the-shooter-save-lives'>[anonymous loser's name replaced to respect victims and prevent future shootings #NoNotoriety]</a>");
-     }
+function walk(node)  
+{
+    
+    var child, next;
+
+    switch ( node.nodeType )  
+    {
+        case 1: 
+        case 9:  
+        case 11: 
+            child = node.firstChild;
+            while ( child ) 
+            {
+                next = child.nextSibling; 
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: 
+            handleText(node);
+            break;
+    }
 }
+
+function handleText(textNode) 
+{
+    var names=['John Russell "Rusty" Houser',"John Russell 'Rusty' Houser","Rusty Houser","John Houser","Omar Mateen","Noah Harpham","Jason Dalton ","Jiverly Wong","James Holmes","Michael McLendon","Seung-Hui Cho","Adam Lanza","Charles Whitman","Eric Harris","Dylan Klebold","Syed Farook","Syed Rizwan Farook","Tashfeen Malik","Dylann Roof","Wade Page","Nidal Hasan","Aaron Alexis","Chris Harper-Mercer","Robert Dear","Robert Lewis Dear"];
+    
+    
+    for(var i=0;i<names.length;i++){
+        var v = textNode.nodeValue;
+        var regex = new RegExp(names[i],'g');
+        v = v.replace(regex, "[anonymous loser's name replaced to respect victims and prevent future shootings #NoNotoriety]");
+        textNode.nodeValue = v;
+    }
+}
+
+
